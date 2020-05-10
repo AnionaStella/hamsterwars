bca44a8ee05c3e54629ccaa19284930aae727ff9
AnionaStella <eiljon88@gmail.com>
2020-05-10 13:11:22 +0200

    added charts api

--- routes/charts.js
+++ routes/charts.js
@@ -8,9 +8,38 @@ const {
 
 const router = new Router();
 
+async function getHamsterArray(orderBy) {
+  let hamsterDocs = await db.collection('hamsters')
+  let fbHamsters = await hamsterDocs.orderBy(orderBy, 'desc').limit(5).get();
 
-router.get('/', (req, res) => {
-  res.send('you are at /charts root')
+  let hamsters = [];
+  fbHamsters.forEach(doc => {
+    hamsters.push(doc.data())
+  })
+  return hamsters;
+}
+
+
+router.get('/top', async (req, res) => {
+  try {
+
+    res.status(200).send(await getHamsterArray("wins"))
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).send(err)
+  }
+})
+
+router.get('/bottom', async (req, res) => {
+  try {
+
+    res.status(200).send(await getHamsterArray("defeats"))
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).send(err)
+  }
 })
 
 module.exports = router;
